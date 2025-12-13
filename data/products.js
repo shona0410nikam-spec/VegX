a13a8a8edc93baa1d21f8142757048c344bf3066
shona0410nikam-spec <shona0410nikam@gmail.com>
2025-12-13 09:23:53 +0530

    Update products.js

--- data/products.js
+++ data/products.js
@@ -1,65 +1,48 @@
 
 const products = [
-
-  // ===== Daily Veg (1kg) =====
+  // Daily Veg (1kg)
   {
     id: 1,
-    category: "Daily Veg",
     name: "Onion",
     weight: "1kg",
     price: 35,
-    image: "/VegX/images/products/Onion.png",
+    image: "images/products/Onion.png"
   },
   {
     id: 2,
-    category: "Daily Veg",
     name: "Potato",
     weight: "1kg",
     price: 30,
-    image: "/VegX/images/products/Potato.png",
+    image: "images/products/Potato.png"
   },
   {
     id: 3,
-    category: "Daily Veg",
     name: "Tomato",
     weight: "1kg",
     price: 35,
-    image: "/VegX/images/products/Tomato.png",
+    image: "images/products/Tomato.png"
   },
 
-  // ===== Cleaned & Chopped (300gm) =====
+  // Cleaned & Chopped (300gm)
   {
     id: 4,
-    category: "Cleaned & Chopped",
     name: "French Beans",
     weight: "300gm",
     price: 50,
-    image: "/VegX/images/products/Cleaned & Chopped French Beans.jpg",
+    image: "images/products/Cleaned & Chopped French Beans.jpg"
   },
   {
     id: 5,
-    category: "Cleaned & Chopped",
     name: "Cluster Beans",
     weight: "300gm",
     price: 60,
-    image: "/VegX/images/products/Cleaned & Chopped Cluster Beans.jpg",
+    image: "images/products/Cleaned & Chopped Cluster Beans.jpg"
   },
   {
     id: 6,
-    category: "Cleaned & Chopped",
     name: "Drumsticks",
     weight: "300gm",
     price: 40,
-    image: "/VegX/images/products/Cleaned & Chopped Drumstick.png",
-  },
-  {
-    id: 7,
-    category: "Cleaned & Chopped",
-    name: "Green Peas",
-    weight: "300gm",
-    price: 55,
-    image: "/VegX/images/products/Cleaned & Shelled Green Peas .jpg",
-  },
+    image: "images/products/Cleaned & Chopped Drumstick.png"
+  }
 ];
-
-export default products;
