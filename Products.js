2a241b4b1526d26a4bbcf2490c636872b5ef2770
shona0410nikam-spec <shona0410nikam@gmail.com>
2025-12-25 05:01:05 +0530

    Create Products.js

---
+++ Products.js
@@ -0,0 +1,30 @@
+const products = [
+  {id:1, name:"Cleaned & Chopped Drumstick", weight:"300gm", price:60, image:"images/products/Cleaned & Chopped Drumstick.png", active:true},
+  {id:2, name:"Potato", weight:"1kg", price:60, image:"images/products/Potato.png", active:true},
+  {id:3, name:"Onion", weight:"1kg", price:40, image:"images/products/Onion.png", active:true},
+  {id:4, name:"Tomato", weight:"500gm", price:30, image:"images/products/Tomato.png", active:true},
+  {id:5, name:"Lemon", weight:"3 pcs", price:20, image:"images/products/Lemon.jpg", active:true},
+
+  {id:6, name:"Cleaned Methi", weight:"300gm", price:60, image:"images/products/Cleaned methi.png", active:true},
+  {id:7, name:"Cleaned & Chopped French Beans", weight:"300gm", price:60, image:"images/products/Cleaned & Chopped French Beans.jpg", active:true},
+  {id:8, name:"Mixed Pulav Vegetables", weight:"300gm", price:60, image:"images/products/Mixed Pulav Vegetables.png", active:true},
+  {id:9, name:"Cleaned Pudina", weight:"300gm", price:20, image:"images/products/Cleaned Pudina.jpg", active:true},
+  {id:10, name:"Cleaned Palak", weight:"200gm", price:60, image:"images/products/Cleaned Palak.png", active:true},
+
+  {id:11, name:"Cleaned Coriander", weight:"100gm", price:20, image:"images/products/Cleaned coriander.png", active:true},
+  {id:12, name:"Cleaned & Chopped Capsicum Green", weight:"300gm", price:60, image:"images/products/Cleaned & Chopped Capsicum Green.jpg", active:true},
+  {id:13, name:"Cleaned & Cut Red Pumpkin", weight:"300gm", price:60, image:"images/products/Cleaned and cut Red Pumpkin.png", active:true},
+  {id:14, name:"Cleaned & Cut Sponge Gourd", weight:"300gm", price:60, image:"images/products/Cleaned & Cut Sponge Gourd.jpg", active:true},
+  {id:15, name:"Cleaned & Chopped Ridge Gourd", weight:"300gm", price:60, image:"images/products/Cleaned & Chopped Ridg Gourd.jpg", active:true},
+
+  {id:16, name:"Cleaned & Shelled Green Peas", weight:"300gm", price:60, image:"images/products/Cleaned & Shelled Green Peas .jpg", active:true},
+  {id:17, name:"Cleaned & Chopped Carrot", weight:"300gm", price:50, image:"images/products/Cleaned & Chopped Carrot .png", active:true},
+  {id:18, name:"Cleaned & Chopped Bottle Gourd", weight:"300gm", price:60, image:"images/products/Cleaned & Chopped Bottle Gourd.png", active:true},
+  {id:19, name:"Cleaned & Chopped Bitter Gourd", weight:"300gm", price:60, image:"images/products/Cleaned & Chopped Bitter Gourd.png", active:true},
+  {id:20, name:"Cleaned & Chopped Cauliflower", weight:"300gm", price:60, image:"images/products/Cleaned & Chopped Cauliflower.jpg", active:true},
+
+  {id:21, name:"Cleaned & Chopped Lady Finger", weight:"300gm", price:50, image:"images/products/Cleaned & Chopped Lady Finger .jpg", active:true},
+  {id:22, name:"Cleaned & Chopped Cluster Beans", weight:"300gm", price:60, image:"images/products/Cleaned & Chopped Cluster Beans.jpg", active:true},
+  {id:23, name:"Fresh Coconut Grated", weight:"200gm", price:60, image:"images/products/Fresh coconut Grated.jpg", active:true},
+  {id:24, name:"Green Chilli", weight:"100gm", price:20, image:"images/products/Green Chilli.png", active:true}
+];
