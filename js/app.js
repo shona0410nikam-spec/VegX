25bc64c572620c1b9f95b0a426e47bc51d7a8554
shona0410nikam-spec <shona0410nikam@gmail.com>
2026-01-09 10:04:06 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,152 +1,34 @@
 const products = [
-  {
-    name: "Drumstick",
-    desc: "cleaned & chopped drumstick<br>weight 300gm<br>₹ 60",
-    price: 60,
-    img: "images/drumstick.png"
-  },
-  {
-    name: "Potato",
-    desc: "potato<br>weight 1kg<br>₹ 40",
-    price: 40,
-    img: "images/potato.png"
-  },
-  {
-    name: "Onion",
-    desc: "onion<br>weight 1kg<br>₹ 40",
-    price: 40,
-    img: "images/onion.png"
-  },
-  {
-    name: "Tomato",
-    desc: "tomato<br>weight 500gm<br>₹ 30",
-    price: 30,
-    img: "images/tomato.png"
-  },
-  {
-    name: "Lemon",
-    desc: "lemon<br>3 pc<br>₹ 20",
-    price: 20,
-    img: "images/lemon.png"
-  },
-  {
-    name: "Methi",
-    desc: "cleaned methi<br>weight 300gm<br>₹ 60",
-    price: 60,
-    img: "images/methi.png"
-  },
-  {
-    name: "French Beans",
-    desc: "cleaned & chopped french beans<br>weight 300gm<br>₹ 60",
-    price: 60,
-    img: "images/french-beans.png"
-  },
-  {
-    name: "Mixed Pulav Vegetables",
-    desc: "mixed pulav vegetables<br>weight 300gm<br>₹ 60",
-    price: 60,
-    img: "images/mixed-pulav-vegetables.png"
-  },
-  {
-    name: "Pudina",
-    desc: "cleaned pudina<br>weight 300gm<br>₹ 20",
-    price: 20,
-    img: "images/pudina.png"
-  },
-  {
-    name: "Palak",
-    desc: "cleaned palak<br>weight 200gm<br>₹ 60",
-    price: 60,
-    img: "images/palak.png"
-  },
-  {
-    name: "Coriander",
-    desc: "cleaned coriander<br>weight 100gm<br>₹ 20",
-    price: 20,
-    img: "images/coriander.png"
-  },
-  {
-    name: "Capsicum",
-    desc: "cleaned & chopped capsicum green<br>weight 300gm<br>₹ 60",
-    price: 60,
-    img: "images/capsicum.png"
-  },
-  {
-    name: "Red Pumpkin",
-    desc: "cleaned and cut red pumpkin<br>weight 300gm<br>₹ 60",
-    price: 60,
-    img: "images/red-pumpkin.png"
-  },
-  {
-    name: "Sponge Gourd",
-    desc: "cleaned & cut sponge gourd<br>weight 300gm<br>₹ 60",
-    price: 60,
-    img: "images/sponge-gourd.png"
-  },
-  {
-    name: "Ridge Gourd",
-    desc: "cleaned & chopped ridge gourd<br>weight 300gm<br>₹ 60",
-    price: 60,
-    img: "images/ridge-gourd.png"
-  },
-  {
-    name: "Green Peas",
-    desc: "cleaned & shelled green peas<br>weight 300gm<br>₹ 60",
-    price: 60,
-    img: "images/green-peas.png"
-  },
-  {
-    name: "Carrot",
-    desc: "cleaned & chopped carrot<br>weight 300gm<br>₹ 50",
-    price: 50,
-    img: "images/carrot.png"
-  },
-  {
-    name: "Bottle Gourd",
-    desc: "cleaned & chopped bottle gourd<br>weight 300gm<br>₹ 60",
-    price: 60,
-    img: "images/bottle-gourd.png"
-  },
-  {
-    name: "Bitter Gourd",
-    desc: "cleaned & chopped bitter gourd<br>weight 300gm<br>₹ 60",
-    price: 60,
-    img: "images/bitter-gourd.png"
-  },
-  {
-    name: "Cauliflower",
-    desc: "cleaned & chopped cauliflower<br>weight 300gm<br>₹ 60",
-    price: 60,
-    img: "images/cauliflower.png"
-  },
-  {
-    name: "Lady Finger",
-    desc: "cleaned & chopped lady finger<br>weight 300gm<br>₹ 50",
-    price: 50,
-    img: "images/lady-finger.png"
-  },
-  {
-    name: "Cluster Beans",
-    desc: "cleaned & chopped cluster beans<br>300gm<br>₹ 60",
-    price: 60,
-    img: "images/cluster-beans.png"
-  },
-  {
-    name: "Grated Coconut",
-    desc: "fresh coconut grated<br>200gm<br>₹ 60",
-    price: 60,
-    img: "images/grated-coconut.png"
-  },
-  {
-    name: "Green Chilli",
-    desc: "green chilli<br>100gm<br>₹ 20",
-    price: 20,
-    img: "images/green-chilli.png"
-  },
-  {
-    name: "Peeled Garlic",
-    desc: "peeled garlic<br>100gm<br>₹ 30",
-    price: 30,
-    img: "images/peeled-garlic.png"
-  }
+  // Normal Vegetables
+  { name: "Potato", price: 40, desc: "weight 1kg", img: "images/potato.png" },
+  { name: "Onion", price: 40, desc: "weight 1kg", img: "images/onion.png" },
+  { name: "Tomato", price: 30, desc: "weight 500gm", img: "images/tomato.png" },
+  { name: "Lemon", price: 20, desc: "3 pc", img: "images/lemon.png" },
+
+  // Leafy Vegetables (cleaned only)
+  { name: "Methi", price: 60, desc: "cleaned – weight 300gm", img: "images/methi.png" },
+  { name: "Pudina", price: 20, desc: "cleaned – weight 300gm", img: "images/pudina.png" },
+  { name: "Palak", price: 60, desc: "cleaned – weight 200gm", img: "images/palak.png" },
+  { name: "Coriander", price: 20, desc: "cleaned – weight 100gm", img: "images/coriander.png" },
+
+  // Cleaned & Chopped Vegetables
+  { name: "Drumstick", price: 60, desc: "cleaned & chopped – weight 300gm", img: "images/drumstick.png" },
+  { name: "French Beans", price: 60, desc: "cleaned & chopped – weight 300gm", img: "images/french-beans.png" },
+  { name: "Capsicum (Green)", price: 60, desc: "cleaned & chopped – weight 300gm", img: "images/capsicum.png" },
+  { name: "Red Pumpkin", price: 60, desc: "cleaned & cut – weight 300gm", img: "images/pumpkin.png" },
+  { name: "Sponge Gourd", price: 60, desc: "cleaned & cut – weight 300gm", img: "images/sponge-gourd.png" },
+  { name: "Ridge Gourd", price: 60, desc: "cleaned & chopped – weight 300gm", img: "images/ridge-gourd.png" },
+  { name: "Carrot", price: 50, desc: "cleaned & chopped – weight 300gm", img: "images/carrot.png" },
+  { name: "Bottle Gourd", price: 60, desc: "cleaned & chopped – weight 300gm", img: "images/bottle-gourd.png" },
+  { name: "Bitter Gourd", price: 60, desc: "cleaned & chopped – weight 300gm", img: "images/bitter-gourd.png" },
+  { name: "Cauliflower", price: 60, desc: "cleaned & chopped – weight 300gm", img: "images/cauliflower.png" },
+  { name: "Lady Finger", price: 50, desc: "cleaned & chopped – weight 300gm", img: "images/lady-finger.png" },
+  { name: "Cluster Beans", price: 60, desc: "cleaned & chopped – weight 300gm", img: "images/cluster-beans.png" },
+
+  // Special
+  { name: "Green Peas", price: 60, desc: "cleaned & shelled – weight 300gm", img: "images/green-peas.png" },
+  { name: "Mixed Pulav Vegetables", price: 60, desc: "cleaned & chopped – weight 300gm", img: "images/pulav-mix.png" },
+  { name: "Fresh Coconut (Grated)", price: 60, desc: "200gm", img: "images/coconut.png" },
+  { name: "Green Chilli", price: 20, desc: "100gm", img: "images/green-chilli.png" },
+  { name: "Garlic", price: 30, desc: "peeled – 100gm", img: "images/garlic.png" }
 ];
